0d0ba6df0971bae642bf6318415eedb3046818af
BuildTools <unconfigured@null.spigotmc.org>
2022-10-24 13:57:16 +0100

    Init

---
+++ src/main/java/beailotus_io/github/multiplyexp/Main.java
@@ -0,0 +1,21 @@
+package beailotus_io.github.multiplyexp;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+    @EventHandler
+    public
+}
